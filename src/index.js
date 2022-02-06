3fe6e4805dc93c2c3b39778ce70acbf873caf665
Real Ivan Nosov <i-am-ivan-nosov@yandex.com>
2022-02-07 02:11:07 +0300

    provider n connect

--- src/index.js
+++ src/index.js
@@ -1,42 +1,25 @@
 import { createStore, bindActionCreators } from "redux";
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-import Counter from "./Counter";
-
+import { Provider } from "react-redux";
 import reducer from "./reducer";
-import * as actions from "./actions";
+import App from "./components/App";
+
+// import * as actions from "./actions";
 
 const store = createStore(reducer);
 
-store.subscribe(() => {
-  console.log(store.getState());
-})
-
-const { dispatch } = store;
+// const { dispatch } = store;
 
 // const bindActionCreator = (creator, dispatch) => (...args) => {
 //   dispatch(creator(...args))
 // }
 
-const { inc, dec, rnd } =
-  bindActionCreators(actions, dispatch);
+// const { inc, dec, rnd } =
+//   bindActionCreators(actions, dispatch);
 
-
-
-const update = () => {
-  ReactDOM.render(
-    <Counter
-      counter={store.getState()}
-      inc={inc}
-      dec={dec}
-      rnd={() => {
-        const rndValue = Math.floor(Math.random() * 10);
-        rnd(rndValue)
-      }} />,
-    document.getElementById('root'))
-}
-
-update();
-store.subscribe(update);
-
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root'))
