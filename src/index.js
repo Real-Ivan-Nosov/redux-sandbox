d80fe576404ba205081411c654b685fd216924c4
Real Ivan Nosov <i-am-ivan-nosov@yandex.com>
2022-02-06 23:38:15 +0300

    payload

--- src/index.js
+++ src/index.js
@@ -3,10 +3,14 @@ import { createStore } from "redux";
 const initialState = 0;
 
 const reducer = (state = 0, action) => {
-  
+
   switch (action.type) {
     case 'INC':
-      return state +=1;
+      return state += 1;
+    case 'DEC':
+      return state -= 1;
+    case 'RND':
+      return state += action.payload;
     default:
       return state;
   }
@@ -17,8 +21,29 @@ store.subscribe(() => {
   console.log(store.getState());
 })
 
+document
+  .getElementById('inc')
+  .addEventListener('click', () => {
+    store.dispatch({ type: 'INC' })
+  });
 
-console.log(store);
-const actio = store.dispatch({ type: 'INC' });
-console.log(actio);
-store.dispatch({ type: 'INC' });
+document
+  .getElementById('dec')
+  .addEventListener('click', () => {
+    store.dispatch({ type: 'DEC' })
+  })
+
+document
+  .getElementById('rnd')
+  .addEventListener('click', () => {
+    const payload = Math.floor(Math.random() * 10);
+    store.dispatch({ type: 'RND', payload: payload })
+  })
+
+const update = () => {
+  document
+    .getElementById('counter')
+    .textContent = store.getState();
+}
+
+store.subscribe(update)
