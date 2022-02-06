3497150399c9e4b47de97283632f59b1b8654994
Real Ivan Nosov <i-am-ivan-nosov@yandex.com>
2022-02-06 23:03:08 +0300

    create store + reducer

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
-console.log('koo');
+import { createStore } from "redux";
+
+const initialState = 0;
+
+const reducer = (state = 0, action) => {
+  
+  switch (action.type) {
+    case 'INC':
+      return state +=1;
+    default:
+      return state;
+  }
+}
+
+const store = createStore(reducer);
+store.subscribe(() => {
+  console.log(store.getState());
+})
+
+
+console.log(store);
+const actio = store.dispatch({ type: 'INC' });
+console.log(actio);
+store.dispatch({ type: 'INC' });
