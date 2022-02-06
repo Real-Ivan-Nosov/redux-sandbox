de852e0bbc28c9da99e229525c32854bfeacf30b
Real Ivan Nosov <i-am-ivan-nosov@yandex.com>
2022-02-07 00:37:14 +0300

    new prj structure

--- src/index.js
+++ src/index.js
@@ -1,22 +1,10 @@
 import { createStore } from "redux";
 
-const initialState = 0;
-
-const reducer = (state = 0, action) => {
-
-  switch (action.type) {
-    case 'INC':
-      return state += 1;
-    case 'DEC':
-      return state -= 1;
-    case 'RND':
-      return state += action.payload;
-    default:
-      return state;
-  }
-}
+import reducer from "./reducer";
+import { inc, dec, rnd } from "./actions";
 
 const store = createStore(reducer);
+
 store.subscribe(() => {
   console.log(store.getState());
 })
@@ -24,20 +12,20 @@ store.subscribe(() => {
 document
   .getElementById('inc')
   .addEventListener('click', () => {
-    store.dispatch({ type: 'INC' })
+    store.dispatch(inc())
   });
 
 document
   .getElementById('dec')
   .addEventListener('click', () => {
-    store.dispatch({ type: 'DEC' })
+    store.dispatch(dec())
   })
 
 document
   .getElementById('rnd')
   .addEventListener('click', () => {
     const payload = Math.floor(Math.random() * 10);
-    store.dispatch({ type: 'RND', payload: payload })
+    store.dispatch(rnd(payload))
   })
 
 const update = () => {
